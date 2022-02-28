090a70af46aedc3a8eac7b8e129e4a6bfac90bc4
João Antonio Soares dos Passos <jdospassos8@gmail.com>
2022-02-28 17:29:27 -0300

    nada demais

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { Public } from 'src/decorators/class.decorator';
 import { UsuarioObj } from 'src/dto/usuario.dto';
 import { UsuarioService } from './usuario.service';
@@ -13,30 +13,30 @@ export class UsuarioController {
     return await this.usuarioCad.cadUsuario(user);
   }
 
-  @Public()
-  @Post('maintenance/on')
-  async setMaintenceOn(@Body() { status: boolean, message: string }) {
-    return await this.usuarioCad.setCacheMaintenance({
-      status: boolean,
-      message: string,
-    });
-  }
+  // @Public()
+  // @Post('maintenance/on')
+  // async setMaintenceOn(@Body() { status: boolean, message: string }) {
+  //   return await this.usuarioCad.setCacheMaintenance({
+  //     status: boolean,
+  //     message: string,
+  //   });
+  // }
 
-  @Public()
-  @Post('maintenance/off')
-  async setMaintenceOff(@Body() { status: boolean, message: string }) {
-    return await this.usuarioCad.setCacheMaintenance({
-      status: boolean,
-      message: string,
-    });
-  }
+  // @Public()
+  // @Post('maintenance/off')
+  // async setMaintenceOff(@Body() { status: boolean, message: string }) {
+  //   return await this.usuarioCad.setCacheMaintenance({
+  //     status: boolean,
+  //     message: string,
+  //   });
+  // }
 
-  @Public()
-  @Get('maintenance/status')
-  async getMaintenceStatus(): Promise<{
-    status: boolean;
-    message: string;
-  }> {
-    return await this.usuarioCad.getCacheMaintenance();
-  }
+  // @Public()
+  // @Get('maintenance/status')
+  // async getMaintenceStatus(): Promise<{
+  //   status: boolean;
+  //   message: string;
+  // }> {
+  //   return await this.usuarioCad.getCacheMaintenance();
+  // }
 }
